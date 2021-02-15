80effa81262631737ed26d0e6a4c8b12b7a6ce1a
root <root@ip-172-31-4-131.us-west-2.compute.internal>
2021-02-15 03:20:39 +0000

    Code miss fixed : consumer.py
     There was on code error in consumer.py .
    Position of try and while are changed so, it's network are disconnected
    really fast.
    So fix this.

--- consumer.py
+++ consumer.py
@@ -1,28 +1,50 @@
 from kafka import KafkaConsumer
 from json import loads
 import time
+from time import sleep
+
 def main(ip_address, port):
     broker_address = ip_address + ":" + port
     consumer = KafkaConsumer( 'test', bootstrap_servers=[broker_address], auto_offset_reset='earliest', enable_auto_commit=True, group_id='my-group', value_deserializer=lambda x: loads(x.decode('utf-8')), consumer_timeout_ms=1000 )
     print('[begin] get consumer list')
+
     before_time = time.time()
-    while(True):
-        try:
+
+    try:
+        while(True):
             current_time = time.time()
             if current_time - before_time > 10:
-                print("About 10 secs passed")
+                print("About 10 secs passed After last message is consumed.")
                 before_time = current_time
+
             for message in consumer:
                 print("Topic: %s, Partition: %d, Offset: %d, Key: %s, Value: %s" % ( message.topic, message.partition, message.offset, message.key, message.value ))
+
+            """
+            message = consumer.poll()
+            if len(message) != 0 : sleep(1)
+            for topic_partition, records in message.items():
+                for record in records:
+                    parsed_record = ujson.loads(record.value)
+                    print(parsed_record)
+            """
+
+
+            before_time = time.time()
     
-        finally:
-            consumer.close()
+    finally:
+        consumer.close()
+
     print('[end] get consumer list')
+
+
 if __name__ == "__main__":
     ip_address = ""
     port = ""
+
     with open("ip_config.txt", "r") as f:
         ip_address = f.readline()
     with open("port.txt", "r") as f:
         port = f.readline()
+
     main(ip_address,port)
